160764e63631f0c016dea5da1c46a967f60a1217
OnurcanCekem <48911241+OnurcanCekem@users.noreply.github.com>
2023-06-30 13:20:16 +0200

    V0.2 color filtering + contour detection
    Filters color (yellow) for bananas and detects all contours (through the area).

---
+++ Fruit_Recognition.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 14:30:31 2023
+
+@author: onurc
+"""
+
+import cv2
+import numpy as np
+#import sys
+
+image = cv2.imread('Banaan1.png')
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+_, binary_image = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+
+#blur
+blur = cv2.GaussianBlur(image,(7,7),1)
+#grayscale van blur
+grayblur = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+
+imgContour = image.copy()
+contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+#cv2.imshow("HSV", hsv_image)
+
+# Color
+
+
+
+# Original starting values
+#lower_yellow = np.array([20, 100, 100])  # Example lower threshold for yellow
+#upper_yellow = np.array([30, 255, 255])  # Example upper threshold for yellow
+
+# Values I want to keep
+#lower_yellow = np.array([10, 50, 70])  # Example lower threshold for yellow
+#upper_yellow = np.array([30, 255, 255])  # Example upper threshold for yellow
+
+lower_yellow = np.array([10, 40, 50])  # Example lower threshold for yellow
+upper_yellow = np.array([30, 255, 255])  # Example upper threshold for yellow
+yellow_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
+segmented_image = cv2.bitwise_and(image, image, mask=yellow_mask)
+binary_image = cv2.bitwise_and(binary_image, binary_image, mask=yellow_mask)
+cv2.imshow("segmented_image", segmented_image)
+
+
+# Filter contours based on area
+min_area = 10  # Minimum area threshold
+max_area = 20000  # Maximum area threshold
+contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# Iterate over the contours and draw bounding boxes around bananas
+for contour in contours:
+    # Ignore small contours
+    if cv2.contourArea(contour) < 1000:
+        continue
+    
+    # Compute the bounding box coordinates
+    x, y, w, h = cv2.boundingRect(contour)
+    
+    # Draw the bounding box on the image
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#print(filtered_contours)
+
+
+"""
+filtered_contours = []
+for contour in contours:
+    area = cv2.contourArea(contour)
+    #if min_area <= area <= max_area:
+    cv2.drawContours(yellow_mask, contour, -1, (0,255,0), 3 )
+    peri = cv2.arcLength( contour, True)
+    rect = cv2.approxPolyDP( contour, 0.02 * peri, True )
+    print (len(rect))
+    x,y,w,h = cv2.boundingRect(rect)
+    cv2.rectangle(yellow_mask, (x, y), (x + w , y + h), (0, 255, 0), 3)
+    filtered_contours.append(contour)
+    cv2.putText(yellow_mask, "points: " + str(len(rect)), (x + w + 20, y + 20), cv2.FONT_HERSHEY_SIMPLEX, .7, (0, 255, 0), 2)
+    cv2.putText(yellow_mask, "area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_SIMPLEX, .7, (0, 255, 0), 2)
+
+
+
+# Contour
+binary_image = cv2.imread('Banaantros1.png', cv2.IMREAD_GRAYSCALE)
+contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Filter contours based on area
+min_area = 1000  # Minimum area threshold
+max_area = 10000  # Maximum area threshold
+
+filtered_contours = []
+for contour in contours:
+    area = cv2.contourArea(contour)
+    if min_area <= area <= max_area:
+        filtered_contours.append(contour)
+"""
+
+cv2.imshow("Display window", image)
+#cv2.imshow("yellow mask", yellow_mask)
+
+
+
+
+
+
+# Press backspace to clear all windows
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
+
+
+#image2 = cv2.imread('Banaantros1.png')
+#cv2.imshow("Display window2", image2)
