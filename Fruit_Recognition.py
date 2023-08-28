2c7036a45737a33dc0bf8183332a98689cfb27a9
OnurcanCekem <48911241+OnurcanCekem@users.noreply.github.com>
2023-08-28 19:08:10 +0200

    V0.7 Brown percentage
    I added brown pixel count and percentage. 
    Also, I tweaked the yellow color threshold to ignore oranges (oranges have a blue pixel of ~10, and bananas have a value of ~100)
    Also, I added a bilateral filter from image_manipulation.py

--- Fruit_Recognition.py
+++ Fruit_Recognition.py
@@ -3,7 +3,7 @@
 Created on Mon Apr 24 14:30:31 2023
 
 @author: onurc
-Version: V0.6
+Version: V0.7
 """
 
 import cv2
@@ -43,40 +43,52 @@ fontColor              = (255,255,255)
 thickness              = 2
 lineType               = 1
 
-image = cv2.imread('Banaan4.png') # read image
-frame_image = image.copy()
 
-# Convert the image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-_, binary_image = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+# Read the image
+image = cv2.imread('Banaan3.png') # read image
+frame_image = image.copy()
 
 # Blur
 blur = cv2.GaussianBlur(image,(7,7),1)
+bilateral = cv2.bilateralFilter(image,9,75,75)
 #cv2.imshow("Blurred image", blur)
+
+# Convert the blur to grayscale
+gray = cv2.cvtColor(bilateral, cv2.COLOR_BGR2GRAY)
+_, binary_image = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+
+# Canny edge detection
+threshold1 = 42
+threshold2 = 104
+canny = cv2.Canny(gray, threshold1, threshold2)
 
 # Grayscale van blur
 grayblur = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
 
+# HSV 
+hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
+# Contour
 imgContour = image.copy()
 contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-cv2.imshow("HSV", hsv_image)
 
-# Values I want to keep
+# Color threshold
 #lower_yellow = np.array([10, 50, 70])  # Example lower threshold for yellow
 #upper_yellow = np.array([30, 255, 255])  # Example upper threshold for yellow
+lower_yellow = np.array([20, 70, 70])  # Example lower threshold for yellow
+upper_yellow = np.array([30, 255, 255])  # Example upper threshold for yellow
+lower_brown = np.array([10, 100, 20])  # Example lower threshold for brown
+upper_brown = np.array([20, 255, 200])  # Example upper threshold for brown
 
-# Testing purposes
-lower_yellow = np.array([10, 40, 50])  # Example lower threshold for yellow (B,G,R)
-upper_yellow = np.array([30, 255, 255])  # Example upper threshold for yellow (B,G,R)
+# Yellow mask (Filter with color range)
+yellow_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow) # Create mask
+segmented_image_yellow = cv2.bitwise_and(image, image, mask=yellow_mask) # Apply yellow mask on original image
+binary_image = cv2.bitwise_and(binary_image, binary_image, mask=yellow_mask) # Apply mask on binary image
 
-# Filter with color range
-yellow_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow)
-segmented_image = cv2.bitwise_and(image, image, mask=yellow_mask)
-binary_image = cv2.bitwise_and(binary_image, binary_image, mask=yellow_mask)
-cv2.imshow("segmented_image (color range)", segmented_image)
-
+# Brown mask (Filter with color range)
+brown_mask = cv2.inRange(hsv_image, lower_brown, upper_brown) # Create mask
+segmented_image_brown = cv2.bitwise_and(image, image, mask=brown_mask) # Apply brown mask on original image
+binary_image2 = cv2.bitwise_and(binary_image, binary_image, mask=brown_mask) # Apply mask on binary image
 
 # Filter contours based on area
 contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -86,8 +98,9 @@ counter = 0
 
 # Iterate over the contours and draw bounding boxes around bananas
 for contour in contours:
+
     # Ignore small contours
-    if cv2.contourArea(contour) < 1000:
+    if cv2.contourArea(contour) < 4000:
         continue
     
     # Compute the bounding box coordinates
@@ -95,33 +108,66 @@ for contour in contours:
     #print(counter, x, w, y, h)
 
     # Check ratio, if rectangle it's likely a banana (ratio is 2:1)
-    if (w*1.3 < h) or (h*1.3 < w): # If banana is found (condition is met)
+    if (w*1.5 < h) or (h*1.5 < w): # If banana is found (condition is met)
+        counter2 = 0
+        # Check which phase banana is in
         
+        # Calculate the area of the contour
+        contour_area = cv2.contourArea(contour)
+        
+        # Create a mask for the current contour
+        contour_mask = np.zeros_like(brown_mask)
+        cv2.drawContours(contour_mask, [contour], 0, 255, thickness=cv2.FILLED)
+        
+        # Calculate the brown pixel count within the contour
+        brown_pixel_count = np.sum(np.logical_and(brown_mask, contour_mask))
+        # Calculate the brown percentage within the contour
+        brown_percentage = (brown_pixel_count / contour_area) * 100
+   
         # Draw the bounding box on the image
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-        # Filter out the already found bananas from frame_image
+        # Filter out the already found bananas from frame_image (useful for multiple bananas)
         inverted_binary = cv2.bitwise_not(binary_image)
         frame_image = cv2.bitwise_and(frame_image, frame_image, mask=inverted_binary) 
         drawtext(x,y-10,"Found!")
         
+        # Draw the contour on the original image
+        cv2.drawContours(image, [contour], 0, (0, 255, 0), 2)
         #binary_image[y:y+h, x:x+w] = (0) # clear already found bananas
 
         # debug locations
         counter +=1
         print(counter, x, w, y, h)
-    else: # If banana is not found
+
+        # Decide which phase based on brown
+        if brown_percentage > 20:
+            print("Phase 3")
+        elif brown_percentage > 12:
+            print("Phase 2")
+        else:
+            print("Phase 1")
+
+        # Display the brown percentage
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        #cv2.putText(image, f"{brown_percentage:.2f}%", (contour[0][0][0], contour[0][0][1]), font, 0.5, (255, 0, 0), 2)
+        drawtext(x,y+20, f"{brown_percentage:.2f}%")
+    
+    #else: # If banana is not found
         # Fill the found square with tuplet's color to make sure it doesn't find it again (50,250,50)
-        image[y:y+h, x:x+w] = (50,250,50)
+        #frame_image[y:y+h, x:x+w] = (50,250,50)
 
 #print(filtered_contours)
-cv2.imshow("Binary image", binary_image)
-
-
 cv2.imshow("original image", image)
+#cv2.imshow("gray", gray)
+#cv2.imshow("HSV", hsv_image)
+#cv2.imshow("Binary image", binary_image)
 cv2.imshow("frame image", frame_image)
 
-#cv2.imshow("yellow mask", yellow_mask)
+#cv2.imshow("yellow (color range)", segmented_image_yellow)
+#cv2.imshow("brown (color range)", segmented_image_brown)
+#cv2.imshow("Canny", canny)
+cv2.imshow("yellow mask", yellow_mask)
 
 # Press backspace to clear all windows
 cv2.waitKey(0)
