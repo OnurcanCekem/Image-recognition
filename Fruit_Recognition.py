f95c3ddb2752e33554d4400cde622cc8a571d223
OnurcanCekem <48911241+OnurcanCekem@users.noreply.github.com>
2023-08-30 11:51:39 +0200

    V0.8 Combined yellow and brown
    I combined the yellow and brown colors. I rearranged the code in order to follow a more linear and readable path, at least to me.
    I also changed the thresholds a bit for brown to work a bit more with black pixels. The same goes for yellow.

--- Fruit_Recognition.py
+++ Fruit_Recognition.py
@@ -3,7 +3,7 @@
 Created on Mon Apr 24 14:30:31 2023
 
 @author: onurc
-Version: V0.7
+Version: V0.8
 """
 
 import cv2
@@ -26,7 +26,7 @@ def printcolor(x, y, thickness):
 # variable x: x-coordinate
 # variable y: y-coordinate
 # variable text: Text to be drawn
-def drawtext(x, y, text):
+def drawtext(x, y, text, fontScale=1):
     cv2.putText(image,text, 
     (x,y-10), 
     font, 
@@ -45,7 +45,7 @@ lineType               = 1
 
 
 # Read the image
-image = cv2.imread('Banaan3.png') # read image
+image = cv2.imread('Banaan4_1.png') # read image
 frame_image = image.copy()
 
 # Blur
@@ -55,47 +55,65 @@ bilateral = cv2.bilateralFilter(image,9,75,75)
 
 # Convert the blur to grayscale
 gray = cv2.cvtColor(bilateral, cv2.COLOR_BGR2GRAY)
-_, binary_image = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+_, binary_image_yellow = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+_, binary_image_brown = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 
 # Canny edge detection
 threshold1 = 42
 threshold2 = 104
 canny = cv2.Canny(gray, threshold1, threshold2)
+ 
 
 # Grayscale van blur
 grayblur = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
 
 # HSV 
 hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+h,s,v = cv2.split(hsv_image)
+hsv_split = np.concatenate((h,s,v),axis=1)
+
+sat_mask = cv2.inRange(s,30,255)
+s = cv2.bitwise_and(s, s, mask=sat_mask)
+#cv2.imshow("Saturation", s)
 
 # Contour
 imgContour = image.copy()
-contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, _ = cv2.findContours(sat_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # Color threshold
 #lower_yellow = np.array([10, 50, 70])  # Example lower threshold for yellow
 #upper_yellow = np.array([30, 255, 255])  # Example upper threshold for yellow
-lower_yellow = np.array([20, 70, 70])  # Example lower threshold for yellow
-upper_yellow = np.array([30, 255, 255])  # Example upper threshold for yellow
-lower_brown = np.array([10, 100, 20])  # Example lower threshold for brown
-upper_brown = np.array([20, 255, 200])  # Example upper threshold for brown
+lower_yellow = np.array([20, 50, 70])  # lower threshold for yellow (example: [10, 50, 70])
+upper_yellow = np.array([30, 255, 255])  # upper threshold for yellow (example:  [30, 255, 255])
+lower_brown = np.array([0, 70, 0])  # lower threshold for brown (example: [10, 100, 20])
+upper_brown = np.array([20, 255, 200])  # upper threshold for brown (example: [20, 255, 200])
 
 # Yellow mask (Filter with color range)
 yellow_mask = cv2.inRange(hsv_image, lower_yellow, upper_yellow) # Create mask
 segmented_image_yellow = cv2.bitwise_and(image, image, mask=yellow_mask) # Apply yellow mask on original image
-binary_image = cv2.bitwise_and(binary_image, binary_image, mask=yellow_mask) # Apply mask on binary image
+binary_image_yellow = cv2.bitwise_and(binary_image_yellow, binary_image_yellow, mask=yellow_mask) # Apply mask on binary image
 
 # Brown mask (Filter with color range)
 brown_mask = cv2.inRange(hsv_image, lower_brown, upper_brown) # Create mask
 segmented_image_brown = cv2.bitwise_and(image, image, mask=brown_mask) # Apply brown mask on original image
-binary_image2 = cv2.bitwise_and(binary_image, binary_image, mask=brown_mask) # Apply mask on binary image
+binary_image_brown = cv2.bitwise_and(binary_image_brown, binary_image_brown, mask=brown_mask) # Apply mask on binary image
+
+# Combine yellow and brown
+color_image = cv2.add(segmented_image_yellow, segmented_image_brown)
+binary_image_combined = cv2.add(binary_image_yellow, binary_image_brown)
+
+# Eroding and dilating
+kernel = np.ones([3,3])
+yellow_dil = cv2.dilate(binary_image_yellow,kernel,1)
+#yellow_erode = cv2.erode(yellow_mask,kernel,1)
+color_dil = cv2.dilate(color_image,kernel,1)
+binary_image_combined = cv2.dilate(binary_image_combined,kernel,1)
 
 # Filter contours based on area
-contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours, _ = cv2.findContours(binary_image_combined, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 # debug
 counter = 0
-
 # Iterate over the contours and draw bounding boxes around bananas
 for contour in contours:
 
@@ -107,10 +125,9 @@ for contour in contours:
     x, y, w, h = cv2.boundingRect(contour)
     #print(counter, x, w, y, h)
 
-    # Check ratio, if rectangle it's likely a banana (ratio is 2:1)
+    # Check ratio, if rectangle it's likely a banana (ratio is roughly 2:1 but depends on image dimensions)
     if (w*1.5 < h) or (h*1.5 < w): # If banana is found (condition is met)
         counter2 = 0
-        # Check which phase banana is in
         
         # Calculate the area of the contour
         contour_area = cv2.contourArea(contour)
@@ -128,9 +145,9 @@ for contour in contours:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
         # Filter out the already found bananas from frame_image (useful for multiple bananas)
-        inverted_binary = cv2.bitwise_not(binary_image)
+        # This currently does nothing important
+        inverted_binary = cv2.bitwise_not(binary_image_combined)
         frame_image = cv2.bitwise_and(frame_image, frame_image, mask=inverted_binary) 
-        drawtext(x,y-10,"Found!")
         
         # Draw the contour on the original image
         cv2.drawContours(image, [contour], 0, (0, 255, 0), 2)
@@ -140,34 +157,42 @@ for contour in contours:
         counter +=1
         print(counter, x, w, y, h)
 
-        # Decide which phase based on brown
-        if brown_percentage > 20:
+        # Decide which phase banana is based on brown
+        if brown_percentage > 50:
+            print("Phase 4")
+        elif brown_percentage > 22:
             print("Phase 3")
-        elif brown_percentage > 12:
+        elif brown_percentage > 8:
             print("Phase 2")
         else:
             print("Phase 1")
 
         # Display the brown percentage
         font = cv2.FONT_HERSHEY_SIMPLEX
-        #cv2.putText(image, f"{brown_percentage:.2f}%", (contour[0][0][0], contour[0][0][1]), font, 0.5, (255, 0, 0), 2)
-        drawtext(x,y+20, f"{brown_percentage:.2f}%")
-    
+        #cv2.putText(image, f"{brown_percentage:.2f}%", (contour[0][0][0], contour[0][0][1]), font, 0.5, (255, 0, 0), 2) # original text code
+        drawtext(x,y+20, f"{brown_percentage:.2f}% brown", 0.5) # Text brown percentage
+        drawtext(x,y+40, f"{w/h:.2f}:1 width:height ratio", 0.5) # Text width:height ratio
+        drawtext(x,y-10,"Found banana!") # Text banana is found
     #else: # If banana is not found
         # Fill the found square with tuplet's color to make sure it doesn't find it again (50,250,50)
         #frame_image[y:y+h, x:x+w] = (50,250,50)
 
+# A list of possible images
 #print(filtered_contours)
 cv2.imshow("original image", image)
+#cv2.imshow("frame image (copy of original)", frame_image)
 #cv2.imshow("gray", gray)
 #cv2.imshow("HSV", hsv_image)
 #cv2.imshow("Binary image", binary_image)
-cv2.imshow("frame image", frame_image)
-
+#cv2.imshow("Binary Combined", binary_image_combined)
+#cv2.imshow("yellow mask", yellow_mask)
+#cv2.imshow("yellow dil image", yellow_dil)
+#cv2.imshow("brown mask", brown_mask)
 #cv2.imshow("yellow (color range)", segmented_image_yellow)
 #cv2.imshow("brown (color range)", segmented_image_brown)
+#cv2.imshow("color (yellow and brown) dilated", color_dil)
+#cv2.imshow("color (yellow and brown)", color_image)
 #cv2.imshow("Canny", canny)
-cv2.imshow("yellow mask", yellow_mask)
 
 # Press backspace to clear all windows
 cv2.waitKey(0)
