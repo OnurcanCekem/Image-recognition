fb7f29d1c484ef65f51028e2c65278eb0da6f292
OnurcanCekem <48911241+OnurcanCekem@users.noreply.github.com>
2023-10-09 16:00:00 +0200

    v0.4 hotfix individual image
    Fixed detect_brown and detect_yellow.
    I also fixed the mean label to work and added a second one. Next up is mean of histogram.

--- GUI_Histrogram.py
+++ GUI_Histrogram.py
@@ -2,7 +2,7 @@
 Created on Mon Oct 02 10:22:39 2023
 
 @author: onurc
-Version: V0.3
+Version: V0.4
 Description: 
 """
 import tkinter as tk
@@ -12,6 +12,11 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+
+global image_index
+global image_global
+image_index = 0
+global mean_image
 
 # Function to close the program
 def close_program():
@@ -31,12 +36,13 @@ def load_previous_image():
         image_index -= 1
         load_image(image_index)
 
-# Function to load and display a specific image
+# Function to load and display a specific image, automated
 def load_image(index):
+    global image_global
     cv2.destroyAllWindows() 
     image_path = image_paths[index]
+    image_global = cv2.imread(image_path) # set global image
     img = Image.open(image_path)
-    print(image_path)
     img.thumbnail((400, 400))  # Resize the image if needed
     photo = ImageTk.PhotoImage(img)
 
@@ -44,17 +50,21 @@ def load_image(index):
     image_label.config(image=photo)
     image_label.image = photo
 
+    #run the tests
+    test1_mean_img()
     image = preprocess_image(image_path)
-    # Display the RGB histogram of the current image
-    display_rgb_histogram(image, index)
+    display_rgb_histogram(image, index) # Display the RGB histogram of the current image
+    test2_mean_histogram()
+    
+    print(image_path)
 
-# Function to load and display a specific image
-def select_image():
+# Function to load and display a specific image, manually
+def select_image_manually():
+    global image_global
     cv2.destroyAllWindows() 
     image_path = filedialog.askopenfilename(title="Select an Image", filetype=(('image files','*.jpg'),('all files','*.*')))
-    image = cv2.imread(image_path)
-    cv2.imshow("Yee",image)
-
+    image_global = cv2.imread(image_path)
+    #cv2.imshow("Yee",image)
     img = Image.open(image_path)
     img.thumbnail((400, 400))  # Resize the image if needed
     photo = ImageTk.PhotoImage(img)
@@ -62,6 +72,14 @@ def select_image():
     # Update the label with the new image
     image_label.config(image=photo)
     image_label.image = photo
+
+    #run the tests
+    test1_mean_img()
+    image = preprocess_image(image_path)
+    display_rgb_histogram(image) # Display the RGB histogram of the current image
+    test2_mean_histogram()
+    
+
     print(image_path)
 
     #image = preprocess_image(image_path)
@@ -69,8 +87,12 @@ def select_image():
     #display_rgb_histogram(image)
 
 # Function to load and preprocess images with multiple feature extraction methods
-def preprocess_image(image_path):
-    image = cv2.imread(image_path)
+def preprocess_image(image_path=0):
+    global image_global
+    if image_path != 0:
+        image = cv2.imread(image_path)
+    else:
+        image = image_global
     image_bilateralblur = cv2.bilateralFilter(image,9,75,75)
     image_grayscaledbilateralblur = cv2.cvtColor(image_bilateralblur, cv2.COLOR_BGR2GRAY)
     _, binary_image_yellow = cv2.threshold(image_grayscaledbilateralblur, 100, 255, cv2.THRESH_BINARY)
@@ -136,44 +158,43 @@ def display_rgb_histogram(image, index=0):
 # Function to detect yellow pixels
 def detect_yellow():
     global image_paths, image_index
-    print(image_index, image_paths)
-    if image_index < len(image_paths):
-        image_path = image_paths[image_index]
-        img = cv2.imread(image_path)
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    global image_global
+    
+    img = image_global
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # Define lower and upper bounds for yellow color in HSV
-        lower_yellow = np.array([15, 50, 70], dtype=np.uint8)
-        upper_yellow = np.array([40, 255, 255], dtype=np.uint8)
+    # Define lower and upper bounds for yellow color in HSV
+    lower_yellow = np.array([15, 50, 70], dtype=np.uint8)
+    upper_yellow = np.array([40, 255, 255], dtype=np.uint8)
 
-        # Create a mask to extract yellow pixels
-        yellow_mask = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
+    # Create a mask to extract yellow pixels
+    yellow_mask = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
 
-        # Display the image with yellow pixels highlighted
-        yellow_highlighted = cv2.bitwise_and(img, img, mask=yellow_mask)
-        cv2.imshow('Yellow Detection', yellow_highlighted)
-        detect_escape_key()
+    # Display the image with yellow pixels highlighted
+    yellow_highlighted = cv2.bitwise_and(img, img, mask=yellow_mask)
+    cv2.imshow('Yellow Detection', yellow_highlighted)
+    #detect_escape_key()
         #cv2.waitKey(0)
         #cv2.destroyAllWindows()
 
 # Function to detect brown pixels
 def detect_brown():
     global image_paths, image_index
-    if image_index < len(image_paths):
-        image_path = image_paths[image_index]
-        img = cv2.imread(image_path)
-        hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # Define lower and upper bounds for brown color in HSV
-        lower_brown = np.array([0, 70, 0], dtype=np.uint8)
-        upper_brown = np.array([20, 255, 200], dtype=np.uint8)
+    global image_global
+    img = image_global
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-        # Create a mask to extract brown pixels
-        brown_mask = cv2.inRange(hsv_img, lower_brown, upper_brown)
+    # Define lower and upper bounds for brown color in HSV
+    lower_brown = np.array([0, 70, 0], dtype=np.uint8)
+    upper_brown = np.array([20, 255, 200], dtype=np.uint8)
 
-        # Display the image with brown pixels highlighted
-        brown_highlighted = cv2.bitwise_and(img, img, mask=brown_mask)
-        cv2.imshow('Brown Detection', brown_highlighted)
+    # Create a mask to extract brown pixels
+    brown_mask = cv2.inRange(hsv_img, lower_brown, upper_brown)
+
+    # Display the image with brown pixels highlighted
+    brown_highlighted = cv2.bitwise_and(img, img, mask=brown_mask)
+    cv2.imshow('Brown Detection', brown_highlighted)
         #detect_escape_key()
         #cv2.waitKey(0)
         #cv2.destroyAllWindows()
@@ -204,6 +225,19 @@ def select_phase(index):
     image_index = -1  # Start from the first image (index 0) when a new folder is selected
     load_next_image()
 
+def test1_mean_img():
+    global image_global
+    global mean_image
+    mean_image = np.mean(image_global)
+    print(mean_image)
+    mean_image_label.config(text = f"Mean image: {mean_image}") #shows as text in the window
+
+def test2_mean_histogram():
+    global image_global
+    global mean_image
+    mean_histogram = np.mean(image_global)
+    mean_histogram_label.config(text = f"Mean histogram: {mean_histogram}") #shows as text in the window
+
 def detect_escape_key():
     while(True):
         c = cv2.waitKey(0)
@@ -226,14 +260,15 @@ previous_button = tk.Button(root, bg='green', text="Previous Image", command=loa
 close_button = tk.Button(root, bg='red', text="Close Program", command=close_program)
 yellow_button = tk.Button(root, bg='yellow', text="Detect Yellow", command=detect_yellow)
 brown_button = tk.Button(root, bg='brown', text="Detect Brown", command=detect_brown)
-individual_image = tk.Button(root, bg='green', text="Select image", command=select_image)
+individual_image = tk.Button(root, bg='green', text="Select image", command=select_image_manually)
 fase1_path = tk.Button(root, bg='green', text="Select phase 1", command=lambda: select_phase(1))
 fase2_path = tk.Button(root, bg='green', text="Select phase 2", command=lambda: select_phase(2))
 fase3_path = tk.Button(root, bg='green', text="Select phase 3", command=lambda: select_phase(3))
 fase4_path = tk.Button(root, bg='green', text="Select phase 4", command=lambda: select_phase(4))
-mean_histogram = 300
-w = tk.Label(root, text=f"Mean: {mean_histogram}") #shows as text in the window
+mean_image_label = tk.Label(root, text="Waiting image...") #shows as text in the window
+mean_histogram_label = tk.Label(root, text="Waiting image...") #shows as text in the window
 
+#mean_label = tk.Label(root, text=f"Mean: {mean_image}") #shows as text in the window
 browse_button.pack(pady=10)
 individual_image.pack(padx=10)
 fase1_path.pack(padx=10)
@@ -245,8 +280,8 @@ next_button.pack(side='right', padx=10)
 yellow_button.pack(pady=10)
 brown_button.pack(pady=10)
 close_button.pack(padx=20, pady=10)
-
-w.pack(padx=20, pady=10)
+mean_image_label.pack(padx=20, pady=10)
+mean_histogram_label.pack(padx=20, pady=10)
 
 # Initialize global variables
 image_paths = []
